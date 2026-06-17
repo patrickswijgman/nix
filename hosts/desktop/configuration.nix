50547fd8b4a06f27b3b26385cdbe84384c2e668c
Patrick <petrik09@live.nl>
2026-06-17 12:29:44 +0200

    chore: update

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -122,6 +122,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    vim
     git
     wl-clipboard
   ];
--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -143,6 +143,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    vim
     git
     wl-clipboard
     openvpn
