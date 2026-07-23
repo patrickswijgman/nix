2d23d8f55e3541da96ba4082f8ed23f5844483a0
Patrick <petrik09@live.nl>
2026-07-23 15:09:18 +0200

    chore: update

--- modules/neovim/plugins/butter-nvim.nix
+++ modules/neovim/plugins/butter-nvim.nix
@@ -7,7 +7,7 @@ vimUtils.buildVimPlugin {
   src = fetchFromGitHub {
     owner = "patrickswijgman";
     repo = "butter.nvim";
-    rev = "00d198bc2615f08c35880d556d000f43c1217728";
-    hash = "sha256-x4dhELQHWhN5U1qyUjgdMp21oiQc6RKE5D7KDlHv6yA=";
+    rev = "3fcc943173a0497aff89bbd3b3e2698ef0f724fa";
+    hash = "sha256-OTQUpfLD8QAcpEbKFO/GXpcCzNLoByz7jobcrTM+X4E=";
   };
 }
